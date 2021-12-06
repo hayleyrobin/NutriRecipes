a41aa598f88f1ceb56233c5027475047914ac583
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2021-12-06 15:51:50 -0500

    Nutrition screen made with swift view controller made

---
+++ NutriRecipies/Nutrients Detail Screen/NutrientsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NutrientsViewController.swift
+//  NutriRecipies
+//
+//  Created by Hayley Robinson on 12/6/21.
+//
+
+import UIKit
+
+class NutrientsViewController: UIViewController {
+
+    @IBOutlet weak var percentValueLabel: UILabel!
+    
+    @IBOutlet weak var servingsLabel: UILabel!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var kcalsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
